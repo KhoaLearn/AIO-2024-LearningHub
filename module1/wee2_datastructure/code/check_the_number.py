bdee1931caa79bbbac2cc5aa6513b7006a66e4b9
KhoaLearn <khoale.maiu@gmail.com>
2024-06-21 22:46:19 +0700

    Fix Sonar

--- module1/wee2_datastructure/code/check_the_number.py
+++ module1/wee2_datastructure/code/check_the_number.py
@@ -1,4 +1,7 @@
 def check_the_number(n):
+    """
+    Kiểm tra xem số n có nằm trong khoảng từ 1 đến 4 hay không.
+    """
     list_of_numbers = []
     result = ""
     for i in range(1, 5):
@@ -12,11 +15,11 @@ def check_the_number(n):
     
     return result
 
-# Kiểm tra hàm với giá trị N = 7
+# Kiểm tra hàm với giá trị n = 7
 n = 7
 assert check_the_number(n) == "False"
 
-# Kiểm tra hàm với giá trị N = 2 và in kết quả
+# Kiểm tra hàm với giá trị n = 2 và in kết quả
 n = 2
 result = check_the_number(n)
 print(result)
